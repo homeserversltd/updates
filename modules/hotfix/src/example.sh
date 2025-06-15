b54b9f7731cc4053912c8de0d1fed58596d613ba
homeserversltd <support@homeservers.ltd>
2025-06-15 22:12:09 +0000

    hotfix now always wins bunch of other json tweaks should push through now

---
+++ modules/hotfix/src/example.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# HOMESERVER Hotfix Test File
+# This is a harmless test file used to verify hotfix functionality
+
+echo "HOMESERVER hotfix test file - $(date)"
+echo "This file was successfully deployed by the hotfix system"
