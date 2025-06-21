e37600d07b2f9bf97232017aefd8d7debaad8a8d
homeserversltd <support@homeservers.ltd>
2025-06-21 22:33:12 +0000

    execute the individual package with main

---
+++ modules/venvs/__main__.py
@@ -0,0 +1,27 @@
+"""
+HOMESERVER Update Management System
+Copyright (C) 2024 HOMESERVER LLC
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+"""
+Main entry point for the venvs module when executed as a package.
+"""
+
+import sys
+from .index import main
+
+if __name__ == "__main__":
+    main(sys.argv[1:] if len(sys.argv) > 1 else []) 
